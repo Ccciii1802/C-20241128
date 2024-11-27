f23a707ec8982d27feff96ed788c91b7e7da2661
chungtsaii777@gmail.com <chungtsaii777@gmail.com>
2024-11-27 23:41:22 +0800

    加入專案檔案。

---
+++ 03_01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_01
+{
+    internal class Program3
+    {
+        static void Main()
+        {
+            // 提示用戶輸入名字
+            Console.WriteLine("請輸入您的名字：");
+            string name = Console.ReadLine(); // 讀取用戶輸入
+
+            // 將名字轉換為大寫
+            string upperName = name.ToUpper();
+
+            // 顯示歡迎訊息
+            Console.WriteLine($"歡迎您，{upperName}!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 04_01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_01
+{
+    internal class Program4
+    {
+        static void Main()
+        {
+            Console.WriteLine("請輸入一種水果");
+
+            // 原始水果名稱
+            string fruit = Console.ReadLine();
+
+            // 複製字串
+            string copiedFruit = string.Copy(fruit);
+
+            // 輸出複製的字串
+            Console.WriteLine($"複製的水果名稱：{copiedFruit}");
+            Console.ReadLine();
+        }
+    }
+}
